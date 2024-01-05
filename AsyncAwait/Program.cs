99ce1973f801ba5b3355db5ecb380dfb1c050a0b
mikepalie <michalis.palierakis@gmail.com>
2024-01-05 17:39:52 +0200

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalispera");
+            Console.WriteLine(GetData().Result);
+            Console.WriteLine("Kalinixta");
+        }
+
+        public async static Task<int> GetData()
+        {
+
+            Task<int> task = new Task<int>(Repository);
+            task.Start();
+
+            int result = await task;
+
+            return  result;
+        } 
+        
+        public static int Repository()
+        {
+            Thread.Sleep(5000);
+            return 2023;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Task<string> task = new Task<string>(GetData);
+            task.Start();
+
+            string result = await task;
+
+            textBox1.Text += result; 
+        }
+
+        public string GetData()
+        {
+            Thread.Sleep(5000);
+            return "Some Text";
+        }
+
+    }
+}
